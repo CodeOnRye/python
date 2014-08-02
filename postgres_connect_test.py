409b64ebd3f27981c59a5b251cb820bbc4289a69
Ryan M <ryan.p.meyer@gmail.com>
2014-08-01 19:00:49 -0500

    Added vars so changing database information is easier

--- postgres_connect_test.py
+++ postgres_connect_test.py
@@ -2,11 +2,15 @@
 #
 # Small script to show PostgreSQL and Pyscopg together
 #
-
 import psycopg2
 
+dbname = "tevatrondb"
+user = "postgres"
+host = "localhost"
+password = "postgres"
+
 try:
-    conn = psycopg2.connect("dbname='tevatrondb' user='postgres' host='localhost' password='postgres'")
+    conn = psycopg2.connect("dbname='%s' user='%s' host='%s' password='%s'" % (dbname, user, host, password))
 except:
     print ("I am unable to connect to the database")
 
